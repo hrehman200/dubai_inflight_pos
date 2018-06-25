403395db735fd07979438e06cf2122e3e575d8b4
hrehman200 <hrehman200@gmail.com>
2018-06-25 13:31:12 +0500

    Refactored revenue calculation code

--- flight_func.php
+++ flight_func.php
@@ -568,3 +568,492 @@ function sendEmail($email, $subject, $body, $include_info_address = false) {
     $response = $mailin->send_email($data);
     return $response;
 }
+
+/**
+ * @param $entity
+ * @return int
+ */
+function getParentEntityId($entity) {
+    global $db;
+
+    $query = $db->prepare('SELECT id FROM business_plan_entities WHERE name = ?');
+    $query->execute([$entity]);
+    if($query->rowCount() > 0) {
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        return $row['id'];
+
+    } else {
+        $query = $db->prepare("INSERT INTO business_plan_entities(parent_id, name) VALUES(?, ?)");
+        $query->execute([0, $entity]);
+        return $db->lastInsertId();
+    }
+}
+
+/**
+ * @param $entity
+ * @param $parent_entity_id
+ * @return int
+ */
+function getBusinessEntityId($entity, $parent_entity_id) {
+    global $db;
+
+    $query = $db->prepare('SELECT id FROM business_plan_entities WHERE name = ?');
+    $query->execute([$entity]);
+    if($query->rowCount() > 0) {
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        return $row['id'];
+
+    } else {
+        $query = $db->prepare("INSERT INTO business_plan_entities(parent_id, name) VALUES(?, ?)");
+        $query->execute([$parent_entity_id, $entity]);
+        return $db->lastInsertId();
+    }
+}
+
+/**
+ * @param $entity_id
+ * @param $year
+ * @param $month
+ * @param $value
+ */
+function updateBusinessEntityValue($entity_id, $year, $month, $value) {
+    global $db;
+
+    $query = $db->prepare('SELECT id FROM business_plan_yearly WHERE business_plan_entity_id = ? AND month = ? AND year = ?');
+    $query->execute([$entity_id, $month, $year]);
+    if($query->rowCount() > 0) {
+        $query = $db->prepare('UPDATE business_plan_yearly SET value = ? 
+          WHERE business_plan_entity_id = ? AND month = ? AND year = ?');
+        $query->execute([$value, $entity_id, $month, $year]);
+
+    } else {
+        $query = $db->prepare("INSERT INTO business_plan_yearly(business_plan_entity_id, month, year, value) 
+          VALUES(?, ?, ?, ?)");
+        $query->execute([$entity_id, $month, $year, $value]);
+    }
+}
+
+/**
+ * @param $arr
+ * @return mixed
+ */
+function sumTwoRows($arr) {
+    if (count($arr) > 1) {
+        for ($i = 1; $i < count($arr); $i++) {
+            $arr[0]['paid'] += $arr[$i]['paid'];
+            $arr[0]['minutes_used'] += $arr[$i]['minutes_used'];
+            $arr[0]['total_minutes'] += $arr[$i]['total_minutes'];
+        }
+
+        array_splice($arr, 1);
+    }
+    return $arr;
+}
+
+/**
+ * @param array $arr
+ * @param callable $key_selector
+ * @return array
+ */
+function array_group_by(array $arr, callable $key_selector) {
+    $result = array();
+    foreach ($arr as $i) {
+        $key = call_user_func($key_selector, $i);
+        $result[$key][] = $i;
+    }
+    return $result;
+}
+
+/**
+ * If search date is 15 May, purchase date is 15 May but credit-flight was taken on 16 May then
+ * credit-used should not be shown on 15 May
+ *
+ * @param $flight_time
+ * @param $date1
+ * @param $date2
+ * @return bool
+ */
+function isTimeInsideSearchedDate($flight_time, $date1, $date2) {
+    $date = (new DateTime($flight_time))->format('Y-m-d');
+    return (strtotime($date) >= strtotime($date1) && strtotime($date) <= strtotime($date2));
+}
+
+/**
+ * Performing sales.date is inside time-range OR booking.flight_time inside time range search at once hang up
+ * the system, thats why two separate queries are needed.
+ *
+ * @param $package_name
+ * @param bool $sale_date_check
+ * @return string
+ */
+function getQuery($package_name, $sale_date_check = true) {
+    if($package_name == 'Skydivers' || $package_name == 'FTF') {
+        $join_with_discount = 'LEFT JOIN discounts d ON fp1.discount_id = d.id OR fp1.discount_id = 0';
+    } else {
+        $join_with_discount = 'INNER JOIN discounts d ON fp1.discount_id = d.id';
+    }
+
+    if($package_name == 'FTF') {
+        $package_check = " fpkg.package_name LIKE 'FTF%'";
+    } else if($package_name == 'RF - Repeat Flights') {
+        $package_check = " fpkg.package_name LIKE 'RF - Repeat Flights%'";
+    } else {
+        $package_check = " fpkg.id IN (6, 8)";
+    }
+
+    if($sale_date_check) {
+        $date_check = 's1.date >= :startDate AND s1.date <= :endDate';
+    } else {
+        $date_check = 'DATE(fb1.flight_time) >= :startDate AND DATE(fb1.flight_time) <= :endDate';
+    }
+
+    $sql = sprintf("SELECT
+                            fp1.id AS flight_purchase_id,
+                            fp1.deduct_from_balance,
+                            fb1.id,
+                            fb1.from_flight_purchase_id,
+                            fb1.flight_time,
+                            IFNULL(fb1.flight_time, NOW()+10) <= NOW() AS flight_taken,
+                            s1.invoice_number,
+                            s1.customer_id,
+                            s1.date,
+                            CASE WHEN(
+                                (s1.mode_of_payment = 'credit_time' OR s1.mode_of_payment_1 = 'credit_time') AND fp1.deduct_from_balance = 2
+                            ) THEN (fb1.duration * c.per_minute_cost) ELSE s1.amount
+                            END AS paid,
+                            CASE WHEN(
+                                s1.mode_of_payment != 'credit_time' AND s1.mode_of_payment_1 != 'credit_time' AND fp1.deduct_from_balance = 0
+                            ) THEN fb1.duration ELSE 0
+                            END AS minutes_used,
+                            CASE WHEN(
+                                s1.mode_of_payment != 'credit_time' AND s1.mode_of_payment_1 != 'credit_time' AND fp1.deduct_from_balance = 0 
+                            ) THEN fo1.duration ELSE 0
+                            END AS total_minutes,
+                            CASE WHEN(
+                                s1.mode_of_payment = 'credit_time' OR s1.mode_of_payment_1 = 'credit_time' OR fp1.deduct_from_balance > 0
+                            ) THEN fb1.duration ELSE 0
+                            END AS credit_used
+                        FROM
+                            sales s1
+                        INNER JOIN flight_purchases fp1 ON
+                            s1.invoice_number = fp1.invoice_id
+                        INNER JOIN flight_offers fo1 ON
+                            fp1.flight_offer_id = fo1.id
+                        INNER JOIN flight_packages fpkg ON
+                            fo1.package_id = fpkg.id
+                        LEFT JOIN flight_bookings fb1 ON
+                            fb1.flight_purchase_id = fp1.id
+                        INNER JOIN customer c ON
+                            fp1.customer_id = c.customer_id
+                        %s
+                        WHERE
+                            %s 
+                            AND(
+                                s1.mode_of_payment IN(
+                                    'Cash',
+                                    'Card',
+                                    'Online',
+                                    'Account',
+                                    'credit_time',
+                                    'credit_cash'
+                                ) OR s1.mode_of_payment_1 IN(
+                                    'Cash',
+                                    'Card',
+                                    'Online',
+                                    'Account',
+                                    'credit_time',
+                                    'credit_cash'
+                                )
+                            ) AND(
+                                %s
+                            ) AND(
+                                (customer_name != 'FDR' AND customer_name != 'MAINTENANCE' AND customer_name != 'inflight staff flying') OR customer_name IS NULL
+                            ) AND d.category ", $join_with_discount, $package_check, $date_check);
+
+    if($package_name == 'Skydivers' || $package_name == 'FTF' || $package_name == 'RF - Repeat Flights') {
+        $sql .= "NOT IN ('Presidential Guard', 'Navy Seal', 'Military')";
+    } else {
+        $sql .= "IN ('".$package_name."')";
+    }
+
+    if($package_name == 'FTF') {
+        $sql .= " AND fpkg.package_name LIKE 'FTF%'";
+    }
+
+    $sql .= " GROUP BY fp1.id, fb1.id";
+    return $sql;
+}
+
+function getDataAndAggregate($package_name) {
+    global $db;
+
+    $sql_w_sale_date = getQuery($package_name);
+    $result = $db->prepare($sql_w_sale_date);
+    $result->execute(array(
+        ':startDate' => $_GET['d1'],
+        ':endDate'   => $_GET['d2']
+    ));
+    $arr2 = $result->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql_w_flight_date = getQuery($package_name, false);
+    $result = $db->prepare($sql_w_flight_date);
+    $result->execute(array(
+        ':startDate' => $_GET['d1'],
+        ':endDate'   => $_GET['d2']
+    ));
+    $arr_flight = $result->fetchAll(PDO::FETCH_ASSOC);
+
+    $arr_diff = array_map('unserialize',
+        array_diff(array_map('serialize', $arr_flight), array_map('serialize', $arr2)));
+
+    $arr2 = array_merge($arr2, $arr_diff);
+
+    $arr_flight_purchase_ids = array_map(function($v) { return $v['flight_purchase_id'];}, $arr2);
+    $arr_flight_purchase_ids = array_unique($arr_flight_purchase_ids);
+
+    $arr_paid = array_group_by($arr2, function($v) { return $v['invoice_number']; });
+    $paid = array_reduce($arr_paid, function($carry, $item) {
+        $carry += $item[0]['paid'];
+        return $carry;
+    });
+
+    $arr_total_minutes = array_group_by($arr2, function($v) { return $v['flight_purchase_id']; });
+    $total_minutes = array_reduce($arr_total_minutes, function($carry, $item) {
+        if(isTimeInsideSearchedDate($item[0]['date'], $_GET['d1'], $_GET['d2'])) {
+            $carry += $item[0]['total_minutes'];
+        }
+        return $carry;
+    });
+
+    $arr_minutes_used = array_group_by($arr2, function($v) { return $v['id']; });
+    $purchased_minutes_used = 0;
+    $total_credit_cost = 0;
+    $minutes_used = array_reduce($arr_minutes_used, function($carry, $item) use (&$purchased_minutes_used, &$total_credit_cost, $arr_flight_purchase_ids, $package_name) {
+        if($item[0]['flight_taken'] == 1) {
+            if ($item[0]['from_flight_purchase_id'] > 0 || $item[0]['deduct_from_balance'] == 2) {
+                if(isTimeInsideSearchedDate($item[0]['flight_time'], $_GET['d1'], $_GET['d2'])) {
+                    $carry += $item[0]['credit_used'];
+
+                    if($item[0]['deduct_from_balance'] == 2) {
+                        $credit_cost_per_minute = getPerMinuteCostForCustomer($item[0]['customer_id']);
+                    } else {
+                        $credit_cost_per_minute = getPerMinuteCostOfPurchasedPackage($item[0]['from_flight_purchase_id']);
+                    }
+                    $total_credit_cost += $credit_cost_per_minute * $item[0]['credit_used'];
+                    // if credit used is from the same purchase in selected time range
+                    if (in_array($item[0]['from_flight_purchase_id'], $arr_flight_purchase_ids)) {
+                        $purchased_minutes_used += $item[0]['credit_used'];
+                    }
+                }
+            } else if(isTimeInsideSearchedDate($item[0]['flight_time'], $_GET['d1'], $_GET['d2'])) {
+                $carry += $item[0]['minutes_used'];
+                $purchased_minutes_used += $item[0]['minutes_used'];
+
+                // special case, customer booked via online on 31st May but came to fly on 1st Jun
+                $credit_cost_per_minute = getPerMinuteCostOfPurchasedPackage($item[0]['flight_purchase_id']);
+                $total_credit_cost += $credit_cost_per_minute * $item[0]['minutes_used'];
+            }
+        }
+        return $carry;
+    });
+
+    $credit_used = array_reduce($arr_minutes_used, function($carry, $item) {
+        if(isTimeInsideSearchedDate($item[0]['flight_time'], $_GET['d1'], $_GET['d2'])) {
+            $carry += $item[0]['credit_used'];
+        }
+        return $carry;
+    });
+
+    if($total_minutes > 0) {
+        $aed_per_paid_minute = $paid / $total_minutes;
+        $total_purchased_cost = $aed_per_paid_minute * $purchased_minutes_used;
+    }
+
+    $arr2 = [[
+        'package_name' => $package_name,
+        'paid' => $paid,
+        'total_minutes' => $total_minutes,
+        'minutes_used' => $minutes_used,
+        'credit_used' => $credit_used,
+        'purchased_minutes_used' => $purchased_minutes_used,
+        'aed_value' => $total_purchased_cost + $total_credit_cost
+    ]];
+
+    return $arr2;
+}
+
+/**
+ * @param $product_name
+ * @param $date1
+ * @param $date2
+ * @return mixed
+ */
+function getMerchandiseRevenue($product_name, $date1, $date2) {
+    global $db;
+
+    if($product_name == TYPE_MERCHANDISE) {
+        $query = $db->prepare('SELECT SUM(s.amount) AS paid
+                            FROM sales s
+                            INNER JOIN sales_order so ON s.invoice_number = so.invoice
+                            INNER JOIN products p ON so.product = p.product_id 
+                            WHERE 
+                            (p.product_name NOT LIKE "%Video%" AND p.product_name NOT LIKE "%Helmet Rent%")
+                            AND p.gen_name = ?
+                            AND (s.date >= ? AND s.date <= ?)');
+        $query->execute([TYPE_MERCHANDISE, $date1, $date2]);
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+
+    } else {
+        $query = $db->prepare('SELECT SUM(s.amount) AS paid
+                            FROM sales s
+                            INNER JOIN sales_order so ON s.invoice_number = so.invoice
+                            INNER JOIN products p ON so.product = p.product_id 
+                            WHERE p.product_name LIKE ? 
+                            AND (s.date >= ? AND s.date <= ?)');
+        $query->execute(["%" . $product_name . "%", $date1, $date2]);
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+    }
+
+    $arr2 = [[
+        'package_name' => $product_name == 'Video' ? 'Videos/Photos' : $product_name,
+        'paid' => $row['paid'],
+        'aed_value' => $row['paid']
+    ]];
+
+    return $arr2;
+}
+
+/**
+ * @param $packages
+ * @param $from
+ * @param $to
+ * @param bool $search_days
+ * @return array
+ */
+function getMinutesFlownInPackages($packages, $from, $to, $search_days = false) {
+    global $db;
+
+    if(count($packages) > 1) {
+        $package_name_check = 'AND (';
+        for ($i = 0; $i < count($packages); $i++) {
+            $package_name_check .= sprintf('fpkg.package_name LIKE "%%%s%%"', $packages[$i]);
+            if ($i != count($packages) - 1) {
+                $package_name_check .= ' OR ';
+            }
+        }
+        $package_name_check .= ')';
+
+    } else {
+        if($packages == ['Skydivers'] || $packages == ['FTF']) {
+            $join_with_discount = 'LEFT JOIN discounts d ON fp.discount_id = d.id OR fp.discount_id = 0';
+        } else {
+            $join_with_discount = 'INNER JOIN discounts d ON fp.discount_id = d.id';
+        }
+
+        if($packages == ['FTF']) {
+            $package_name_check = "AND fpkg.package_name LIKE 'FTF%'";
+        } else if($packages == ['Skydivers']) {
+            $package_name_check = "AND fpkg.id IN (6, 8)";
+        }
+
+        if($packages == ['Skydivers'] || $packages == ['FTF']) {
+            $discount_category_check = "AND d.category NOT IN ('Presidential Guard', 'Navy Seal', 'Military')";
+        } else {
+            $discount_category_check = "AND d.category IN ('Presidential Guard', 'Navy Seal', 'Military')";
+        }
+    }
+
+    $sql = sprintf('SELECT DISTINCT fb.id, fpkg.package_name, fb.duration AS duration 
+                        FROM flight_bookings fb
+                        INNER JOIN flight_purchases fp ON fb.flight_purchase_id = fp.id
+                        INNER JOIN flight_offers fo ON fp.flight_offer_id = fo.id
+                        INNER JOIN flight_packages fpkg ON fo.package_id = fpkg.id
+                        %s
+                        WHERE
+                          DATE(fb.flight_time) >= :from AND DATE(fb.flight_time) <= :to
+                          %s
+                          %s
+                        ', $join_with_discount, $package_name_check, $discount_category_check);
+
+    if($search_days == 'weekends') {
+        $sql .= ' AND (DAYNAME(fb.flight_time) = "Friday" OR DAYNAME(fb.flight_time) = "Saturday")';
+    } else if($search_days == 'weekdays') {
+        $sql .= ' AND (DAYNAME(fb.flight_time) != "Friday" AND DAYNAME(fb.flight_time) != "Saturday")';
+    }
+
+    //$sql .= ' GROUP BY fpkg.package_name';
+
+    $query = $db->prepare($sql);
+    $query->execute([
+        ':from' => $from,
+        ':to' => $to
+    ]);
+    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $arr = [];
+    foreach($rows as $row) {
+        if(isset($arr[$row['package_name']])) {
+            $arr[$row['package_name']] += $row['duration'];
+        } else {
+            $arr[$row['package_name']] = $row['duration'];
+        }
+    }
+
+    // we need to sum same packages e.g. FTF-Single, FTF-Multipe into FTF
+    if($search_days) {
+        $arr2 = [];
+        for($i=0; $i<count($packages); $i++) {
+            foreach($arr as $package_full_name=>$minutes_used) {
+
+                if($packages[$i] == 'Skydivers' && strpos($package_full_name, 'Experienced') !== false) {
+                    $arr2[$packages[$i]] += $minutes_used;
+
+                } else if($packages[$i] == 'Military' && strpos($package_full_name, 'Experienced') !== false) {
+                    $arr2[$packages[$i]] += $minutes_used;
+
+                } else if(strpos($package_full_name, $packages[$i]) !== false) {
+                    $arr2[$packages[$i]] += $minutes_used;
+                }
+            }
+        }
+        return $arr2;
+    }
+
+    return $arr;
+}
+
+/**
+ * @param $from
+ * @param $to
+ * @return array
+ */
+function getFlightDiscountsGiven($from, $to) {
+    global $db;
+
+    $query = $db->prepare('SELECT d.category, d.percent, SUM((d.percent*fp.price/100)) AS discount_value
+                        FROM `flight_purchases` fp
+                        INNER JOIN discounts d ON fp.discount_id = d.id
+                        WHERE DATE(fp.created) >= ? AND DATE(fp.created) <= ?
+                        GROUP BY d.category');
+    $query->execute([$from, $to]);
+    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+}
+
+/**
+ * @param $from
+ * @param $to
+ * @return array
+ */
+function getMerchandiseDiscountsGiven($from, $to) {
+    global $db;
+
+    $query = $db->prepare('SELECT d.category, d.percent, SUM((d.percent*so.price/100)) AS discount_value 
+                        FROM `sales_order` so
+                        INNER JOIN discounts d ON so.discount_id = d.id
+                        WHERE so.date >= ? AND so.date <= ? AND d.percent > 0
+                        GROUP BY d.category');
+    $query->execute([$from, $to]);
+    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+}
