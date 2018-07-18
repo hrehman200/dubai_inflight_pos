3a81d1e127680d4753332272bbcbf1cd66d7348f
hrehman200 <hrehman200@gmail.com>
2018-07-18 16:07:49 +0500

    Removed older jquery version to fix post data via ajax

--- main/collect_meraas.php
+++ main/collect_meraas.php
@@ -1,87 +1,7 @@
-<html>
 <?php
-require_once('auth.php');
+include('header.php');
 ?>
-<head>
-    <title>
-        POS
-    </title>
-    <link href="css/bootstrap.css" rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="css/DT_bootstrap.css">
-
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <style type="text/css">
-        body {
-            padding-top: 60px;
-            padding-bottom: 40px;
-        }
-
-        .sidebar-nav {
-            padding: 9px 0;
-        }
-
-        @media print {
-            .sidebar-nav, .navbar, .span2, .breadcrumb, .contentheader, .btns, form {
-                display: none;
-            }
-            .span10, #tblSalesReport {
-                display: block;
-                width: 120%;
-                margin-left: -20%;
-            }
-        }
-    </style>
-    <link href="css/bootstrap-responsive.css" rel="stylesheet" media="all">
-
-
-    <link href="../style.css" media="screen" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" type="text/css" href="tcal.css"/>
-    <script type="text/javascript" src="tcal.js"></script>
-    <script language="javascript">
-        function Clickheretoprint() {
-            $('#tblSalesReport').closest('.span10').removeClass()
-        }
-    </script>
-
-
-    <script language="javascript" type="text/javascript">
-        /* Visit http://www.yaldex.com/ for full source code
-         and get more free JavaScript, CSS and DHTML scripts! */
-        var timerID = null;
-        var timerRunning = false;
-        function stopclock() {
-            if (timerRunning)
-                clearTimeout(timerID);
-            timerRunning = false;
-        }
-        function showtime() {
-            var now       = new Date();
-            var hours     = now.getHours();
-            var minutes   = now.getMinutes();
-            var seconds   = now.getSeconds()
-            var timeValue = "" + ((hours > 12) ? hours - 12 : hours)
-            if (timeValue == "0") timeValue = 12;
-            timeValue += ((minutes < 10) ? ":0" : ":") + minutes
-            timeValue += ((seconds < 10) ? ":0" : ":") + seconds
-            timeValue += (hours >= 12) ? " P.M." : " A.M."
-            document.clock.face.value = timeValue;
-            timerID                   = setTimeout("showtime()", 1000);
-            timerRunning              = true;
-        }
-        function startclock() {
-            stopclock();
-            showtime();
-        }
-        window.onload = startclock;
-    </SCRIPT>
-
-    <style>
-        tr:nth-child(even) {background: #FFF}
-    </style>
-</head>
-<body>
-<?php include('navfixed.php'); ?>
 <div class="container-fluid">
     <div class="row-fluid">
         <div class="span2">
@@ -388,7 +308,6 @@ require_once('auth.php');
 </div>
 
 </body>
-<script src="js/jquery.js"></script>
 <script type="text/javascript">
 
     $(function() {
@@ -407,11 +326,11 @@ require_once('auth.php');
                 'call': 'emailSalesReportToAdmin',
                 'tableHtml': $('#tblSalesReport').parent().html()
             },
-            dataType: 'json',
+            dataType: "json",
             success: function (response) {
                 alert('Email sent');
                 window.top.close();
-            }
+            },
         });
         <?php
         }
